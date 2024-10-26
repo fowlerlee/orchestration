e915b0e11eae4cb58a34478d2950a4f2f2a8f9d0
Lee Fowler <fowler.lee8@gmail.com>
2024-10-27 00:07:54 +0200

    added  logging and test for leader election and reelection

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -6,7 +6,8 @@ import (
 )
 
 func TestElectionAndFailure(t *testing.T) {
-	setManagers := []string{"8080", "8081", "8082", "8083", "8084"}
+	l := "localhost:"
+	setManagers := []string{l + "8080", l + "8081", l + "8082", l + "8083", l + "8084"}
 	m0 := MakeManager(setManagers[0])
 	m1 := MakeManager(setManagers[1])
 	m2 := MakeManager(setManagers[2])
@@ -15,12 +16,29 @@ func TestElectionAndFailure(t *testing.T) {
 	m1.OtherManagers = setManagers
 	m2.OtherManagers = setManagers
 	m3.OtherManagers = setManagers
+	m0.StartManagerRPC()
+	m1.StartManagerRPC()
+	m2.StartManagerRPC()
+	m3.StartManagerRPC()
+
 	m0.StartLeaderElection()
 	if m0.CheckForLeader() {
-		fmt.Printf("Leader is : %v", m0)
-	}
-	if m1.CheckForLeader() {
-		fmt.Printf("Leader is : %v", m1)
+		fmt.Printf("State of replica m0: %v\n", m0.State)
+		fmt.Printf("State of replica m1: %v\n", m1.State)
+		fmt.Printf("State of replica m2: %v\n", m2.State)
+		fmt.Printf("State of replica m3: %v\n", m3.State)
 	}
 
+	m0.StopManagerRPC()
+	m1.StartLeaderElection()
+	if m1.CheckForLeader() {
+		fmt.Printf("State of replica m0: %v\n", m0.State)
+		fmt.Printf("State of replica m1: %v\n", m1.State)
+		fmt.Printf("State of replica m2: %v\n", m1.State)
+		fmt.Printf("State of replica m3: %v\n", m1.State)
+	}
+
+	m1.StopManagerRPC()
+	m2.StopManagerRPC()
+	m3.StopManagerRPC()
 }
